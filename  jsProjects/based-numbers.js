1ba88269f0559f69997aa6e3f191e98402610324
joseardila <joseatw12@gmail.com>
2025-01-19 22:07:13 -0500

    Working on Binary Numbers

---  jsProjects/based-numbers.js
+++  jsProjects/based-numbers.js
@@ -2,13 +2,78 @@ let myNumber = 10;
 
 let binary = "";
 
-while ( myNumber > 0 ) {
+while (myNumber > 0) {
+
+    let remainder = myNumber % 2;
+
+    binary = remainder + binary;
 
     myNumber = Math.floor(myNumber / 2);
 
-    binary = myNumber + 0;
+} console.log(binary);
 
-    console.log(binary);
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (myNumber > 0) {
+//
+//     myNumber = Math.floor(myNumber / 2);
+//
+// } if (myNumber % 2 === 0) {
+//
+//     myNumber = Math.floor(myNumber / 2);
+//
+//     binary = "0";
+//
+//
+// } if (myNumber % 2 != 0) {
+//
+//     myNumber = Math.floor(myNumber / 2);
+//
+//     binary = "1";
+//
+// } console.log(binary);
+
+
+
+
+
+
+
+
+
+
+// for (let i = myNumber; i >= 0; i--) {
+//
+//     let i = Math.floor(myNumber / 2 );
+//
+//     if (i % 2 == 0) {
+//
+//        binary = 0;
+//
+//
+//     }
+//
+//     if (i % 2 != 0) {
+//
+//         binary = 1;
+//
+//
+//     }
+// console.log(binary);
+//     // console.log(i);
+// }
 
