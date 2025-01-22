8be67477c42712631de6b45eb8993d47563a83c7
joseardila <joseatw12@gmail.com>
2025-01-22 00:00:34 -0500

    Got it Glory to God

---  jsProjects/index.js
+++  jsProjects/index.js
@@ -11,6 +11,12 @@ let multiple = 5;
 const numbers = [0,1,2,3,4,5,6,7,8,9];
 
 for (let i = 0; i < numbers.length; i++) {
-    let ans = numbers.length * multiple;
-    console.log(ans);
+    let ans = numbers[i] * multiple;
+    console.log(multiple,"*", numbers[i],"=",ans);
 }
+
+// const musicNotes = ["Do", "Re", "Mi", "Fa", "So", "La", "Ti"];
+//
+// for (let i = 0; i < musicNotes.length; i++) {
+//     console.log(musicNotes[i]);
+// }
