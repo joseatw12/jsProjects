712053dd82d9927110297503bad7c08e0b1e7f02
joseardila <joseatw12@gmail.com>
2025-01-27 00:15:19 -0500

    .Index of and .includes

---
+++ newIndex.js
@@ -0,0 +1,24 @@
+const characters = [
+    "The Wally Watchers",
+    "Wilma",
+    "Fritz",
+    "Wizard Whitebeard",
+    "Odlaw",
+    // "Waldo",
+    "Woof"
+];
+
+let waldoIndex = (characters.indexOf("Waldo"));
+
+// console.log("Found Waldo at index", waldoIndex,"!");
+
+if (waldoIndex > -1) {
+
+    console.log("Found Waldo at index", waldoIndex,"!");
+
+} else {
+
+    console.log("Not found");
+
+}
+
